7cea791f7eafb67df984c6262a4ad3bebbe22d39
joshimayank312 <joshimayankh312@gmail.com>
2024-04-05 23:12:21 +0530

    styling react component using style-component and css module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+// import styled from "styled-components"; // styled-components is used to give unique className
+import styles from "./Button.module.css"; // react css module also used to give unique className by simply changing name from 'Button.css' to  'Button.module.css'.
 
-import './Button.css';
+// // This css specified like this bcoz here we are using styled-components
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-const Button = props => {
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//   $:focus {
+//     outline: none;
+//   }
+
+//   $:hover,
+//   $:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
